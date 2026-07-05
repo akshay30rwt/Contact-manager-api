560b4b513d7ca059902e703d21385aaaec3a8742
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-05 16:40:20 +0530

    feat: server setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const errorHandler = require('./middleware/errorHandler');
+const authRoutes = require('./routes/authRoutes');
+const contactRoutes = require('./routes/contactRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(errorHandler);
+
+app.use('/auth', authRoutes);
+app.use('/contacts', contactRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Connection failed', error);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
