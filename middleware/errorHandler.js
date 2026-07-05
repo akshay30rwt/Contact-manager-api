a7bd6bba53f29812f061145f24eb11457610421c
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-05 13:06:07 +0530

    fix: update errorHandler to handle jwt errors

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,14 @@
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
 
+    if (err.name === 'TokenExpiredError') {
+        err.statusCode = 401;
+        err.message = 'Token has expired. Please log in again.';
+    } else if (err.name === 'JsonWebTokenError') {
+        err.statusCode = 401;
+        err.message = 'Invalid token.';
+    }
+
     const message = err.message || 'Internal Server Error';
     const statusCode = err.statusCode || 500;
 
